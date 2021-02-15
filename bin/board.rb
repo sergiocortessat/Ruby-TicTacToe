cf61936b07b6770cb9a2d30ed04bd7db72f7bc67
ari karim <arikarim146@gmail.com>
2021-02-15 13:09:31 +0300

    added update_board function

--- bin/board.rb
+++ bin/board.rb
@@ -8,6 +8,7 @@ class Board
   def initialize
     @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
+
   def show
     puts "#{cells[0]} | #{cells[1]} | #{cells[2]}"
     puts '---+---+---'
@@ -15,7 +16,17 @@ class Board
     puts '---+---+---'
     puts "#{cells[6]} | #{cells[7]} | #{cells[8]}"
   end
+
+  def update_board(number, symbol)
+    cells[number] = symbol
+  end
+
+  def valid_move?(number)
+    cells[number - 1] = number
+  end
+
 end
 
 game = Board.new
-game.show
+# game.show
+game.valid_move?(1)
