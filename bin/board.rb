553f8e58674a0285e70fa17318b7116106688ec1
ari karim <arikarim146@gmail.com>
2021-02-15 12:37:17 +0300

    added board to tictactoe

---
+++ bin/board.rb
@@ -0,0 +1,21 @@
+class Board
+  attr_reader :cells
+
+  WINNING_COMBOS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ].freeze
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  def show
+    puts "#{cells[0]} | #{cells[1]} | #{cells[2]}"
+    puts '---+---+---'
+    puts "#{cells[3]} | #{cells[4]} | #{cells[5]}"
+    puts '---+---+---'
+    puts "#{cells[6]} | #{cells[7]} | #{cells[8]}"
+  end
+end
+
+game = Board.new
+game.show
