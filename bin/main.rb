7dc41e6e72557f6883bc94d2a209a05f3dc6bc66
ari karim <arikarim146@gmail.com>
2021-02-16 16:54:13 +0300

    added main user_interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
-require_relative "board"
+# require_relative 'board'
+require_relative 'display'
+require_relative 'game'
+require_relative 'board'
 
-# TicTacToe
-
+startt = Game.new
+startt.play
